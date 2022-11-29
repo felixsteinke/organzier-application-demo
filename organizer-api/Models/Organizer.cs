9784f2f8f85c11572fbe7b89ed72f53a03dcd562
FlorianGrabowski <flo.grabowski.98@gmail.com>
2022-11-29 12:32:11 +0100

    add Organizer Model

---
+++ organizer-api/Models/Organizer.cs
@@ -0,0 +1,14 @@
+﻿namespace organizer_api.Models
+{
+    public class Organizer
+    {
+        public int Id { get; set; }
+        public string? Titel { get; set; }
+        public long? EntryDate { get; set; }
+        public double? Estimate { get; set; }
+        public int? Priority { get; set; }
+        public long? DueDate { get; set; }
+        public bool? Done { get; set; }
+        public string? Description { get; set; }
+    }
+}
