318e6cc527185189fe71149a089b88a38bc34326
felixsteinke <steinke.felix@yahoo.de>
2022-12-15 10:53:46 +0100

    style calendar picker

--- organizer-ui/src/app/components/calendar/calendar-picker/calendar-picker.component.ts
+++ organizer-ui/src/app/components/calendar/calendar-picker/calendar-picker.component.ts
@@ -1,6 +1,11 @@
 import {Component, EventEmitter, Input, OnChanges, OnInit, Output, SimpleChanges} from '@angular/core';
 import {CalendarDay, DateRange} from "../../../models/calendar";
 
+interface MonthItem {
+  monthOfYear: number;
+  name: string;
+  calendarDays: number;
+}
 
 @Component({
   selector: 'app-calendar-picker[year]',
@@ -9,9 +14,27 @@ import {CalendarDay, DateRange} from "../../../models/calendar";
 })
 export class CalendarPickerComponent implements OnInit, OnChanges {
 
+  monthRow1: MonthItem[] = [
+    {monthOfYear: 1, name: 'JAN', calendarDays: 1},
+    {monthOfYear: 2, name: 'FEB', calendarDays: 2},
+    {monthOfYear: 3, name: 'MAR', calendarDays: 3},
+    {monthOfYear: 4, name: 'APR', calendarDays: 4},
+    {monthOfYear: 5, name: 'MAY', calendarDays: 5},
+    {monthOfYear: 6, name: 'JUN', calendarDays: 6},
+  ];
+  monthRow2: MonthItem[] = [
+    {monthOfYear: 7, name: 'JUL', calendarDays: 7},
+    {monthOfYear: 8, name: 'AUG', calendarDays: 8},
+    {monthOfYear: 9, name: 'SEP', calendarDays: 9},
+    {monthOfYear: 10, name: 'OCT', calendarDays: 10},
+    {monthOfYear: 11, name: 'NOV', calendarDays: 11},
+    {monthOfYear: 12, name: 'DEC', calendarDays: 12},
+  ];
+
   @Input() year: number | undefined;
   minDate: Date | undefined;
   maxDate: Date | undefined;
+
   @Input() calendarDays: CalendarDay[] = [];
   @Output() selectedRange: EventEmitter<DateRange> = new EventEmitter<DateRange>();
 
